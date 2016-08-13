5b16e2180596cbf01360ed867dd9f7f9e84cfd4e
blenderon <blenderon@gmail.com>
2016-08-13 15:59:22 +0300

    scanner+search+drawer

--- app/src/main/java/org/vscan/ResultFragment.java
+++ app/src/main/java/org/vscan/ResultFragment.java
@@ -1,10 +1,7 @@
 package org.vscan;
 
 import android.app.Fragment;
-import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,8 +24,57 @@ public class ResultFragment extends Fragment {
             CustomCaptureActivity.barcodeView.setVisibility(View.GONE);
         }
 
-        TextView v2 = (TextView) getActivity().findViewById(R.id.te);
-        v2.setText(te);
+        Product product = DBHelper.getProduct(CustomCaptureActivity.db,te);
+
+        ImageView im = (ImageView) getActivity().findViewById(R.id.im);
+        ImageView im1 = (ImageView) getActivity().findViewById(R.id.im2);
+        ImageView im2 = (ImageView) getActivity().findViewById(R.id.im3);
+        TextView te0 = (TextView) getActivity().findViewById(R.id.te0);
+        TextView te1 = (TextView) getActivity().findViewById(R.id.te1);
+        TextView te2 = (TextView) getActivity().findViewById(R.id.te2);
+        TextView te3 = (TextView) getActivity().findViewById(R.id.te3);
+        TextView te4 = (TextView) getActivity().findViewById(R.id.te4);
+        TextView te5 = (TextView) getActivity().findViewById(R.id.te5);
+        TextView aaa = (TextView) getActivity().findViewById(R.id.aaa);
+        im2.setVisibility(View.GONE);
+        aaa.setVisibility(View.GONE);
+        te5.setVisibility(View.GONE);
+
+        if(product == null) {
+            im.setImageResource(R.drawable.sad_cow);
+            te0.setText("עדיין לא רשום");
+        } else { // 7290000300139 =>  7290015673198
+            if(product.is_vegan.equals("לא")) {
+                im.setImageResource(R.drawable.sad_cow);
+                te0.setText("לא טבעוני");
+                te1.setText("מוצר: "+product.name);
+                te2.setText("יצרן: "+product.company);
+                if(product.barcode.equals("7290000300139")) {
+                    im1.setImageResource(R.drawable.p1);
+                    im2.setVisibility(View.VISIBLE);
+                    im2.setImageResource(R.drawable.p2);
+                    aaa.setVisibility(View.VISIBLE);
+
+                } else {
+                    if(product.barcode.equals("7290015673198")) {
+                        im1.setImageResource(R.drawable.p2);
+                    } else {
+                        im1.setImageResource(R.drawable.p3);
+                    }
+                }
+//                te3.setText(product.food_type);
+//                te4.setText(product.product_type);
+                te5.setText("מכיל: "+product.why_not);
+                te5.setVisibility(View.VISIBLE);
+            } else {
+                im.setImageResource(R.drawable.happy_cow);
+                te0.setText("טבעוני");
+                te1.setText("מוצר: "+product.name);
+                te2.setText("יצרן: "+product.company);
+//                te3.setText(product.food_type);
+//                te4.setText(product.product_type);
+            }
+        }
 
         return v;
     }
